b066ce0676a2c4673bb102f04c056e0abec7fc58
DevVictor19 <antoniovictor12@live.com>
2024-08-10 22:30:20 -0300

    feat: initial setup

---
+++ src/main/java/com/devvictor/spring_boot_refresh_token/SpringBootRefreshTokenApplication.java
@@ -0,0 +1,13 @@
+package com.devvictor.spring_boot_refresh_token;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRefreshTokenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRefreshTokenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devvictor/spring_boot_refresh_token/SpringBootRefreshTokenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devvictor.spring_boot_refresh_token;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRefreshTokenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
