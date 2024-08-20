d6193f26ee3d49ab96891961cd9c5a96cc161da4
DevVictor19 <antoniovictor12@live.com>
2024-08-19 22:28:32 -0300

    feat: add repository annotation

--- src/main/java/com/devvictor/spring_boot_refresh_token/repositories/PermissionRepository.java
+++ src/main/java/com/devvictor/spring_boot_refresh_token/repositories/PermissionRepository.java
@@ -2,5 +2,7 @@ package com.devvictor.spring_boot_refresh_token.repositories;
 
 import com.devvictor.spring_boot_refresh_token.entities.Permission;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PermissionRepository extends JpaRepository<Permission, Long> {}
--- src/main/java/com/devvictor/spring_boot_refresh_token/repositories/UserRepository.java
+++ src/main/java/com/devvictor/spring_boot_refresh_token/repositories/UserRepository.java
@@ -3,9 +3,11 @@ package com.devvictor.spring_boot_refresh_token.repositories;
 import com.devvictor.spring_boot_refresh_token.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     UserDetails findByUsername(String username);
     Optional<User> findByEmail(String email);
